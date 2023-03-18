f09abfb013429504432e238c6df8bf14e1ca4c85
Kasprzak-Arkadiusz <Akasprzak016@gmail.com>
2023-03-18 11:08:40 +0100

    Add a simple arguments validator

---
+++ src/Extensions/ConsoleWriter.cs
@@ -0,0 +1,18 @@
+﻿namespace PyCSS_parser.Extensions;
+
+public static class ConsoleWriter
+{
+    private const ConsoleColor ErrorColor = ConsoleColor.Red;
+    private const ConsoleColor InformationColor = ConsoleColor.Green;
+
+    private static void Write(string message, ConsoleColor color)
+    {
+        var previousForegroundColor = Console.ForegroundColor;
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ForegroundColor = previousForegroundColor;
+    }
+
+    public static void WriteError(string message) => Write(message, ErrorColor);
+    public static void WriteInformation(string message) => Write(message, InformationColor);
+}
---
+++ src/Validators/ArgumentsValidator.cs
@@ -0,0 +1,36 @@
+﻿using PyCSS_parser.Extensions;
+
+namespace PyCSS_parser.Validators;
+
+public static class ArgumentsValidator
+{
+    private const int ExpectedNumberOfArguments = 1;
+
+    public static bool AreCorrect(string[] args)
+    {
+        return NumberOfArgumentsMatch(args.Length) && FileExists(args[0]);
+    }
+
+    private static bool NumberOfArgumentsMatch(int numberOfArguments)
+    {
+        if (numberOfArguments == ExpectedNumberOfArguments)
+        {
+            return true;
+        }
+
+        ConsoleWriter.WriteError(
+            $"Invalid number of arguments. Expected {ExpectedNumberOfArguments}, received {numberOfArguments}");
+        return false;
+    }
+
+    private static bool FileExists(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            return true;
+        }
+
+        ConsoleWriter.WriteError("File does not exist");
+        return false;
+    }
+}
