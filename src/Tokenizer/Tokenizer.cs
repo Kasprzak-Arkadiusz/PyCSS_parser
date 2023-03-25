3cb970c01b23a81c4d29b15798ebfd80f6a43588
Kasprzak-Arkadiusz <Akasprzak016@gmail.com>
2023-03-25 19:07:40 +0100

    Improve the regular expression to split the file content

--- src/Tokenizer/Tokenizer.cs
+++ src/Tokenizer/Tokenizer.cs
@@ -12,31 +12,9 @@ public class Tokenizer : ITokenizer
         }
 
         var unifiedFileContent = fileContent.ReplaceLineEndings("\n");
-        var tokens = Regex.Split(unifiedFileContent, @"(?<=[ |\n|\t])").ToList();
-        SplitNewLineCharacters(tokens);
-
+        unifiedFileContent = Regex.Replace(unifiedFileContent, @"(\S)?([\,\+\>\<\:])(\S)?", "$1 $2 $3");
+        var tokens = Regex.Split(unifiedFileContent, @" +|(\n)|(\t)").Where(s => s != string.Empty).ToList();
+        
         return tokens.AsReadOnly();
-    }
-
-    private static void SplitNewLineCharacters(IList<string> tokens)
-    {
-        for (var index = 0; index < tokens.Count; index++)
-        {
-            var token = tokens[index];
-            if (token.Length == 1)
-            {
-                continue;
-            }
-
-            if (token[^1] != '\n')
-            {
-                tokens[index] = token.Trim();
-                continue;
-            }
-
-            tokens[index] = token.TrimEnd('\n');
-            tokens.Insert(index + 1, "\n");
-            index++;
-        }
     }
 }
